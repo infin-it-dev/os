2a89ecb7653e25b5e3380dcfd5150cf546d70758
「 」Data <rylanhughey@gmail.com>
2024-06-12 22:58:43 -0500

    Created basic Kernel and added run script for debian systems.

--- boot.asm
+++ boot.asm
@@ -4,9 +4,39 @@
 mov [BOOT_DISK], dl
 
 
+xor ax, ax
+mov es, ax
+mov ds, ax
+mov bp, 0x8000
+mov sp,bp
+
+mov bx, KERNEL_LOCATION
+mov dh, 2
+
+mov ah, 0x02
+mov al, dh
+mov ch, 0x00
+mov dh, 0x00
+mov cl, 0x02
+mov dl, [BOOT_DISK]
+int 0x13
+
+
+
+
+
+mov ah, 0x0
+mov al, 0x3
+int 0x10
+
+
 
 CODE_SEG equ  GDT_code - GDT_start
 DATA_SEG  equ GDT_data -GDT_start
+
+
+
+
 
 cli
 lgdt [GDT_descriptor]
@@ -16,6 +46,8 @@ mov cr0, eax
 jmp CODE_SEG:start_protected_mode
 
 jmp $
+
+BOOT_DISK: db 0
 
 
 GDT_start:
@@ -48,10 +80,21 @@ GDT_descriptor:
 
 [bits 32]
 start_protected_mode:
-    mov al, 'A'
-    mov ah, 0x0f
-    mov [0xb8000], ax
-    jmp $
+    mov ax, DATA_SEG
+        mov ds, ax
+        mov ss, ax
+        mov es, ax
+        mov fs, ax
+        mov gs, ax
+
+
+        mov ebp, 0x90000
+
+
+        mov esp, ebp
+
+        jmp KERNEL_LOCATION
+
 
 BOOT_DISK: db 0
 
